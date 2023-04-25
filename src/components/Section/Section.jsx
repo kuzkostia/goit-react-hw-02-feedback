a8a740036adaad8d9783308a51c3b30ecbc56739
kuzkostia <112246522+kuzkostia@users.noreply.github.com>
2023-04-25 13:05:36 +0300

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -12,7 +12,7 @@ const Section = ({ title, children }) => {
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
-  
+  children: PropTypes.node.isRequired,
 };
 
 export default Section;
